cb07fcb5014a40ac14a36a273a966d3a22c26f94
Marco <28888207+Marco3jp@users.noreply.github.com>
2024-06-05 15:18:13 +0000

    コピーしたことがわかるようにdialogを出す

--- projects/emoji-picker/src/main.ts
+++ projects/emoji-picker/src/main.ts
@@ -1,9 +1,21 @@
 import './style.css'
 import {Picker} from 'emoji-mart'
 
+let clearId: number | null = null;
+const dialog = <HTMLDialogElement>document.querySelector("#copied-dialog")
+
 // @ts-ignore
 async function copyEmoji(emoji) {
+  // 見栄えびみょ～かもしれんがちらつくくらいならマシかな～という気持ちで削除キャンセルする
+  if (clearId) clearTimeout(clearId)
+
   await navigator.clipboard.writeText(emoji.native);
+  dialog.show()
+
+  clearId = setTimeout(()=> {
+    clearId = null;
+    dialog.close()
+  }, 1500)
 }
 
 const pickerOptions = {
